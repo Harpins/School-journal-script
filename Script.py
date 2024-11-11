08ae3a0c5e2e4d789e7a22ce910abe446bd7ae8d
Igor Derevnin <shroom2207@yandex.ru>
2024-11-11 21:41:42 +0300

    Add files via upload

--- Script.py
+++ Script.py
@@ -2,48 +2,43 @@ import random
 from datacenter.models import Chastisement, Commendation, Lesson, Mark, Schoolkid
 
 PRAISES = ['Отличит червя от опарыша',
-              'Умеет умножать ноль на ноль',
-              'Плодоносит остроумием',
-              'Лихой юнец',
-              'Вел переписку с Вольтером',
-              'Подарил юродивым бархатные колпаки и часть Сибири',
-              'Установил компактные личные границы',
-              'Эмоционально вырос в конце',
-              'Сечет тему',
-              'Он особенный',
-              'Знания помножают радость',
-              ]
-
-LESSONS = ['Краеведение',
-           'География',
-           'Математика',
-           'Музыка',
-           'Физкультура',
-           'Изобразительное искусство',
-           'Технология',
-           'Русский язык',
-           'Литература',
-           'Обществознание',
-           'Иностранный язык',
-           'Биология ',
-           'История',
-           'Основы безопасности жизнедеятельности (ОБЖ)',
+           'Умеет умножать ноль на ноль',
+           'Плодоносит остроумием',
+           'Лихой юнец',
+           'Вел переписку с Вольтером',
+           'Подарил юродивым бархатные колпаки и часть Сибири',
+           'Установил компактные личные границы',
+           'Эмоционально вырос в конце',
+           'Сечет тему',
+           'Он особенный',
+           'Знания помножают радость',
            ]
+
+SUBJECTS = ['Краеведение',
+            'География',
+            'Математика',
+            'Музыка',
+            'Физкультура',
+            'Изобразительное искусство',
+            'Технология',
+            'Русский язык',
+            'Литература',
+            'Обществознание',
+            'Иностранный язык',
+            'Биология ',
+            'История',
+            'Основы безопасности жизнедеятельности (ОБЖ)',
+            ]
 
 
 def fix_marks(kid):
     all_marks = Mark.objects.filter(schoolkid=kid)
-    bad_marks = all_marks.filter(points__in=[2, 3])
-    for mark in bad_marks:
-        mark.points = 5
-        mark.save()
-    return
+    all_marks.filter(points__in=[2, 3]).update(points=5)
 
 
 def fix_chastisements(kid):
-    chasts = Chastisement.objects.filter(schoolkid=kid)
-    chasts.delete()
-    return
+    chastisements = Chastisement.objects.filter(schoolkid=kid)
+    chastisements.delete()
 
 
 def create_commendation(kid, lesson):
@@ -51,34 +46,36 @@ def create_commendation(kid, lesson):
                                 created=lesson.date,
                                 schoolkid=kid,
                                 subject=lesson.subject,
-                                teacher=lesson.teacher)
-    return
+                                teacher=lesson.teacher,
+                                )
+
+
+def check_name(name):
+    kid = []
+    try:
+        kid = Schoolkid.objects.get(full_name__contains=name)
+    except Schoolkid.DoesNotExist:
+        print('Проверьте корректность ввода имени')
+    except Schoolkid.MultipleObjectsReturned:
+        print('В списке более одного ученика с таким именем. Введите имя в формате "Фамилия Имя Отчество"')
+    return kid
 
 
 def main():
     your_name = 'Фролов Иван'
+    kid = check_name(your_name)
     try:
-        Schoolkid.objects.get(full_name__contains=your_name)
-    except Schoolkid.DoesNotExist:
-        print('Проверьте корректность ввода имени')
+        year_of_study = kid.year_of_study
+        group_letter = kid.group_letter
+    except AttributeError:
         return
-    except Schoolkid.MultipleObjectsReturned:
-        print('В списке более одного ученика с таким именем. Введите имя в формате "Фамилия Имя Отчество"')
-        return
-
-    kid = Schoolkid.objects.filter(full_name__contains=your_name)[0]
-    year_of_study = kid.year_of_study
-    group_letter = kid.group_letter
-    lessons_in_this_class = Lesson.objects.filter(
-        year_of_study=year_of_study, group_letter=group_letter)
-
-    lesson = random.choice(LESSONS)
-    recent_lesson = lessons_in_this_class.filter(
-        subject__title=lesson).order_by('-date').first()
-    if recent_lesson == None:
+    subject = random.choice(SUBJECTS)
+    recent_lesson = Lesson.objects.filter(year_of_study=year_of_study,
+                                          group_letter=group_letter,
+                                          subject__title=subject,
+                                          ).order_by('-date').first()
+    if not recent_lesson:
         raise Exception('Проверьте корректность названия урока')
-
     fix_marks(kid)
     fix_chastisements(kid)
     create_commendation(kid, recent_lesson)
-
